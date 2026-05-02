39ba00fadce6cab4958290b3fa457267df216e05
Rudy <vegetable.sose@gmail.com>
2026-05-01 20:00:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import Database from "better-sqlite3";
 
 const app = express();
-const db = new Database("app.db");
+const db = new Database("./Database/app.db");
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
